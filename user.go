da470866dc814b38d050e5b580fc8aeae94a6404
qiliangliu <1921532406@qq.com>
2022-01-07 16:13:46 +0800

    v0.5 在线用户查询

--- user.go
+++ user.go
@@ -58,8 +58,21 @@ func (this *User) Offline() {
 	this.server.BroadCast(this, "下线")
 }
 
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
 //用户消息处理功能
 func (this *User) DoMessage(msg string) {
-	//将用户消息进行广播
-	this.server.BroadCast(this, msg)
+	if msg == "who" {
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else {
+		//将用户消息进行广播
+		this.server.BroadCast(this, msg)
+	}
 }
