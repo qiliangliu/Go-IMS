1b095e3b1b69dbeaf4a5cb7276b3fc13c13d7f8f
qiliangliu <1921532406@qq.com>
2022-01-07 13:47:48 +0800

    v0.2 用户功能上线

--- main.go
+++ main.go
@@ -3,4 +3,5 @@ package main
 func main() {
 	server := NewServer("127.0.0.1", 8888)
 	server.Start()
+
 }
--- server.go
+++ server.go
@@ -3,26 +3,69 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的 channel
+	Message chan string
 }
 
 //创建一个server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
+}
+
+//广播消息方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
 }
 
 //处理当前链接的业务
 func (this *Server) Handler(conn net.Conn) {
 	//...当前链接的业务
-	fmt.Println("链接建立成功...")
+	// fmt.Println("链接建立成功...")
+
+	//创建一个用户对象
+	user := NewUser(conn)
+
+	//User上线，将用户加到 OnlineMap 中去
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线
+	this.BroadCast(user, "已上线")
+
+	//阻塞 handler 结束
+	select {}
+}
+
+//监听Message管道中的一个gorountine, 一旦这个管道中有消息，我们就把这个消息广播到OnlineMap中所用用户
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将所有的消息推送给在线用户
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
 }
 
 //启动服务器的接口
@@ -36,6 +79,9 @@ func (this *Server) Start() {
 
 	//close listen socket
 	defer listener.Close()
+
+	//启动监听进程
+	go this.ListenMessager()
 
 	//accept
 	for {
