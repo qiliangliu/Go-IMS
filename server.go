5e927cdf28de6a4de778f1e58d7f8d7c06970074
qiliangliu <1921532406@qq.com>
2022-01-07 17:03:09 +0800

    v0.7 增加超时强踢功能

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -45,6 +46,7 @@ func (this *Server) Handler(conn net.Conn) {
 
 	//User上线
 	user.Online()
+	isLive := make(chan bool)
 
 	//接受客户端发送的消息
 	go func() {
@@ -66,10 +68,25 @@ func (this *Server) Handler(conn net.Conn) {
 			//将用户的消息进行广播
 			user.DoMessage(msg)
 		}
+
+		//向isLive管道中发送一个任意消息，以刷新用户活跃
+		isLive <- true
 	}()
 
 	//阻塞 handler 结束
-	// select {}
+	for {
+		select {
+		case <-isLive:
+			//当前用户是活跃的，应该重置定时器
+			//不做任何事情，为了激活select，更新下的计时器
+		case <-time.After(time.Second * 600):
+			//已经超时，将当前用户强制关闭，并释放资源
+			user.SendMsg("你被踢了\n")
+			close(user.C) //关闭管道
+			conn.Close()  //释放连接
+			return        //runtime.Goexit()
+		}
+	}
 }
 
 //监听Message管道中的一个gorountine, 一旦这个管道中有消息，我们就把这个消息广播到OnlineMap中所用用户
